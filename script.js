76f66be3ac99413da5869ed122384e2b9574e9df
Revanth Sai Chaparala <98806472+revanthsaich@users.noreply.github.com>
2024-07-11 21:08:46 +0530

    upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const btn = document.getElementById('button');
+const text = document.getElementById('input');
+const form = document.getElementById('entry');
+const container = document.getElementById('container');
+const del = document.getElementById('delete');
+form.addEventListener('submit',function(event){
+    event.preventDefault();
+    console.log(text.value);
+    const value=input.value;
+    add(value);
+    text.value="";
+});
+
+function add(value){
+    container.innerHTML=`
+            <div class="list-item">
+                <div class="action">
+                    <p>${value}</p>
+                </div>
+                <div class="delete">
+                    <i class="fa-solid fa-trash"></i>                
+                </div>
+            </div>
+`+container.innerHTML;
+}
+
+container.addEventListener('click', function(event) {
+    
+        const listItem = event.target.closest('.list-item');
+        if (listItem) {
+            listItem.remove(); 
+        }
+    
+});
